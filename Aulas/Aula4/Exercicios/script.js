a12e1838c9b611d072072313f22dbef0bf1237fc
Leandra Lemos <leandralemos960@gmail.com>
2025-09-23 19:21:23 -0300

    Adicionei o arquivo Aulas

---
+++ Aulas/Aula4/Exercicios/script.js
@@ -0,0 +1,5 @@
+const meuNome = "Leandra,"
+const meuSobrenome = "Lemos"
+let minhaIdade = "18 anos"
+let souEstudante = true
+console.log("Meu nome é", meuNome, " já o meu sobrenome é", meuSobrenome, "minha idade é", minhaIdade, "e eu sou estudante", souEstudante)
---
+++ Aulas/Aula4/exemplo1/script.js
@@ -0,0 +1,15 @@
+/* Exemplo 1 */
+
+//  Leandra Lemos.
+
+console.log ("BAnaNA?!!!")
+
+
+prompt ("What is your name?")
+prompt ("Quantos anos você tem?")
+
+
+const nomeVariavel = "Leandra"
+const minhaIdade = 18
+
+console.log ("O seu nome é:", nomeVariavel, "e sua idade é:", minhaIdade)
