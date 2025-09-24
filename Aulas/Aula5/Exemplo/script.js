cafc4b8e8973b3c3ac4e6bf7ccb1a66d4bdaef81
Leandra Lemos <leandralemos960@gmail.com>
2025-09-23 21:47:03 -0300

    Adicionei os arquivos das aulas 3 e 5

---
+++ Aulas/Aula5/Exemplo/script.js
@@ -0,0 +1,27 @@
+const got = "Game Of Thrones"
+const numeroTemporadas = 8
+console.log (got)
+console.log (numeroTemporadas)
+console.log (typeof(got))
+console.log (typeof(numeroTemporadas))
+
+let novaVariavel
+console.log (novaVariavel)
+novaVariavel = 22
+console.log (novaVariavel)
+novaVariavel = undefined
+console.log (novaVariavel)
+
+let minhaVariavel 
+console.log (minhaVariavel)
+minhaVariavel = null
+console.log (minhaVariavel)
+
+const idade = prompt ("Qual é a sua idade?")
+console.log (typeof (idade))
+//Number() conversor de string para number
+const idadeNumber = Number (idade)
+console.log (typeof idadeNumber)
+//String() conversor de Number para String
+const idadeTexto = idadeNumber.toString()
+console.log (typeof (idadeTexto))
---
+++ Aulas/Aula5/Exercicios/script.js
@@ -0,0 +1,4 @@
+const nomePessoa = prompt ("Qual é o seu nome?")
+const idadePessoa = prompt ("Qual é a sua idade")
+console.log (typeof (nomePessoa))
+console.log (typeof (idadePessoa))
