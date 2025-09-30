fda48e5fba37fd97617f8040842992799f031a0d
Leandra Lemos <leandralemos960@gmail.com>
2025-09-29 21:56:45 -0300

    Adicionei os arquivos das aulas recentes

---
+++ Aulas/Aula5/Revisão/script.js
@@ -0,0 +1,65 @@
+/* 1- let  a = 10
+      let b = 10 
+      console.log (b)
+    
+      b = 5
+      console.log(a, b) */
+      
+/* Let é uma variavél que pode ser mudada, como podemos obersar nesses dois codigos,
+inicialmente se trata de um codigo em que a e b valem dez, em seguida imprimiu b como dez,
+apos redefiniu b para cinco e imprimiu a e b.
+
+console.log (10)
+console. log (10, 5)
+
+*/
+
+/* 2- let a2 = 10
+      let b2 = 20
+      c = a2
+      b2 = c
+      a2 = b2
+     console.log (a, b, c)
+
+Foram definidas duas variaveis a e b, em seguida adicionada uma c com o valor igual
+a variavel a, apos isso os valores de a e b foram mudados, a ficando com o valor de b
+e b ficando com o mesmo valor que c.
+
+console. log (20, 10, 10)
+
+*/
+
+let a = 10
+let b = 10
+console.log (b)
+b = 5
+console.log (a, b)
+
+let a2 = 10
+let b2 = 20
+c = a2
+b2 = c
+a2 = b2
+console.log (a2, b2, c)
+
+/* 3 - let horasTrabalho = prompt ("Quantas horas voce trabalha por dia?")
+       let salarioDiario = prompt ("Quanto voce recebe por dia?")
+       console.log ("Voce recebe", salarioDiario,"/", horasTrabalho, "por hora") */
+
+let horasTrabalho = prompt ("Quantas horas voce trabalha por dia?")
+let salarioDiario = prompt ("Quanto voce recebe por dia?")
+console.log ("Voce recebe", salarioDiario,"/", "por hora", horasTrabalho)
+
+/* 1 - */
+
+let nome 
+let idade
+console.log (typeof (nome), typeof (idade))
+// Foi impresso para descobrir o valor da variavel
+nome = prompt ("Qual seu nome?")
+idade = prompt ("Qual sua idade?")
+
+
+
+
+
---
+++ Aulas/Aula6/Exemplos/script.js
@@ -0,0 +1,33 @@
+const primeiroNumero = 10
+const segundoNumero = 20
+
+const resultado = primeiroNumero + segundoNumero + 4
+console.log  (resultado)
+
+
+console.log (primeiroNumero - segundoNumero)
+
+const primeiroValor = 10
+const segundoValor = 25
+const novoResultado = primeiroValor * segundoValor
+console.log (novoResultado)
+
+console.log (primeiroValor/segundoValor)
+
+let minhaIdade = 17
+minhaIdade = minhaIdade + 1
+console.log (minhaIdade)
+
+let resultadoSimples = 100
+resultadoSimples += 50
+console.log (resultadoSimples)
+
+resultadoSimples -= 50
+console.log (resultadoSimples)
+
+resultadoSimples *= 50
+console.log (resultadoSimples)
+
+resultadoSimples /= 50
+console.log (resultadoSimples)
+
