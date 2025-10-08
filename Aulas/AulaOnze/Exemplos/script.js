62c7bc34b8bfc534bb829c5bdeda0c3878d7791b
Leandra Lemos <leandralemos960@gmail.com>
2025-10-07 21:48:27 -0300

    Adicionei a AulaOnze

---
+++ Aulas/AulaOnze/Exemplos/script.js
@@ -0,0 +1,23 @@
+const pokemon = ["bulbasaur","squirtle","charmander","sceptile","farfetc","kadrabra","hunter","mimikyu","joltik"]
+console.log(pokemon.length)
+
+const seriesBoas = ["Supernatural","TheBigBangTeory"]
+console.log(seriesBoas.includes("Vikings"), seriesBoas.includes("Supernatural"))
+
+const numeros = [1,2,3,4]
+numeros.push(5)
+console.log(numeros)
+numeros.push(6,7,8)
+console.log(numeros)
+
+const meusPeixes = ["palhaço","mandarim","esturjão"]
+meusPeixes.pop()
+console.log(meusPeixes)
+
+
+const letras = ["a","b","c","d","e","f","g","h"]
+letras.splice(2,1)
+console.log(letras)
+letras.splice(3,2)
+console.log(letras)
+
---
+++ Aulas/AulaOnze/Exercicios/script.js
@@ -0,0 +1,37 @@
+// EXER. 1 -
+
+const minhaArray = ["Munchkin","MaineCoon","Siamês","Persa","Ragdoll","Sphynx","Siberiano"]
+const escolha = Number(prompt("Usuario, escolha um numero?"))
+console.log(minhaArray[escolha])
+
+// EXER. Aleatorio
+
+const Valores = ["1","2","3","4","5","6"]
+Valores.lenght
+console.log(Valores)
+Valores.push(7)
+console.log(Valores)
+Valores.splice(3,2)
+console.log(Valores)
+Valores.length
+console.log(Valores)
+
+// EXER. de FIXAÇÂO 1 -
+
+let array 
+console.log ("a.", array) // a. Undefined
+array = null
+console.log("b.", array) // b. null
+array = [3,4,5,6,7,8,9,10,11,12,13]
+console.log("c.", array.length) // c. 11
+let i = 0
+console.log("d.", array[i]) // d. 3
+array[i+1] = 19
+console.log("e.", array) // e. 
+const valor = array[i+6]
+console.log("f.", valor) // f. 
+
+/* Cada uma dessas variaveis esta determinando,
+o valor de de si mesmas (por cada letra) atraves das arrays. */
+
+
